4ddea89b96071c130f6fdefb7f513a9bad3d986c
mabb0512 <mariana.bravo@gmail.com>
2019-01-29 20:58:07 -0500

    adding .env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var env = require('dotenv').load();
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -16,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+mongoose.connect(process.env.MONGODB_URI || `mongodb://${process.env.user}:${process.env.password}@ds213513.mlab.com:13513/nytreact-mabb`);
 
 // Start the API server
 app.listen(PORT, function() {
